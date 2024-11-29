61f2882761ed89cc8c7004644578e4cbb7007e78
João P. Carvalho <joao.carvalho315@etec.sp.gov.br>
2024-11-29 09:15:36 -0300

    Update menu.js

--- Landing Page/menu.js
+++ Landing Page/menu.js
@@ -1,13 +1,3 @@
-window.addEventListener("scroll", function() {
-    let header = document.querySelector('header');
-    if (header) {
-        header.classList.toggle('scroll', window.scrollY > 0);
-    }
-    let conta = document.querySelector('.conta');
-    if (conta) {
-        conta.classList.toggle('scroll', window.scrollY > 0);
-    }
-});
 
 window.addEventListener("load", function() {
     document.body.classList.add("loaded");
